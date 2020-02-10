077f5d0e0075ba1117857cc6dc8a9e3d845c4d8c
max-student <max-student@maxtrain.com>
2020-02-10 09:12:58 -0500

    Strings

---
+++ StringTutorial/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StringTutorial {
+    class Program {
+        static void Main(string[] args) {
+            
+            var st1 = new StringTutorialClass();
+            var fullname = st1.Fullname();
+            Console.WriteLine($"Fullname is {fullname}");
+            Console.WriteLine($"Fullname2 is {st1.Fullname2()}");
+
+            var Denise = new StringTutorialClass("Denise", "Bartick");
+            Console.WriteLine($"Denise {Denise.Fullname()}");
+            Console.WriteLine($"Denise {Denise.Fullname2()}");
+
+            }
+        }
+    }
---
+++ StringTutorial/StringTutorialClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringTutorial {
+    class StringTutorialClass {
+
+        public string Firstname;
+        public string Lastname;
+
+        public StringTutorialClass() {
+            Firstname = "Sarah";
+            Lastname = "Bode";
+        }
+
+        public StringTutorialClass(string firstname, string lastname) {
+            Firstname = firstname;
+            Lastname = lastname;
+            }
+
+        public string Fullname() {
+            //var Firstname = "Sarah";
+            //var Lastname = "Bode";
+            var fullname = $"{Lastname}, {Firstname}";
+            return fullname;
+        }
+
+        public string Fullname2() {
+            //var Firstname = "Sarah";
+            //var Lastname = "Bode";
+
+            var fullname = $"{Firstname} {Lastname}";
+            return fullname;
+        }
+    }
+}
